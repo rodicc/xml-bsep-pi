49a453005f72edbb85f803b6f7c47ca070312eb5
milos ostojic <ostojicmilos93@gmail.com>
2018-02-08 22:26:52 +0100

    Ispravljene tekstualne greske

--- banke/src/main/java/ftn/xmlwebservisi/banke/BankeApplication.java
+++ banke/src/main/java/ftn/xmlwebservisi/banke/BankeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages= {"service, helpers"})
-@EnableJpaRepositories("respository")
+@EnableJpaRepositories("repository")
 @EntityScan("model")
 public class BankeApplication {
 
--- banke/src/main/java/repository/NalogZaPlacanjeRepository.java
+++ banke/src/main/java/repository/NalogZaPlacanjeRepository.java
@@ -4,13 +4,16 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import model.NalogZaPlacanje;
 
 public interface NalogZaPlacanjeRepository extends CrudRepository<NalogZaPlacanje, Integer>{
 
-	@Query("from NalogZaPlacanje n " +
-		   "where n.datumNaloga = datum naloga AND (n.racunDuznika = :brojRacuna OR n.racunPrimaoca = :brojRacuna")
+	@Query("select n " +
+		   "from NalogZaPlacanje n " +
+		   "where n.datumNaloga = :datumNaloga AND (n.racunDuznika = :brojRacuna OR n.racunPrimaoca = :brojRacuna)")
 	public List<NalogZaPlacanje> nadjiPoDatumuIBrojuRacuna(Date datumNaloga, String brojRacuna);
 }
