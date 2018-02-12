5e1be10a836273658c0ffd8a9a6bd5d68b13d581
bogdan-todorovic <bogdantodorovic93@gmail.com>
2018-02-12 18:02:18 +0100

    zalutali error

--- banke/src/main/java/xml/ftn/banke/SoapEndpoint.java
+++ banke/src/main/java/xml/ftn/banke/SoapEndpoint.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import service.Servis;
 
