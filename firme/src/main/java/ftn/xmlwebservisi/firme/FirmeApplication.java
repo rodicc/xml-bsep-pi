ab6eef3ef9c749d72fab354066b8b27e96403978
bogdan-todorovic <bogdantodorovic93@gmail.com>
2018-06-27 00:04:44 +0200

    log4j2 configuration

--- firme/src/main/java/ftn/xmlwebservisi/firme/FirmeApplication.java
+++ firme/src/main/java/ftn/xmlwebservisi/firme/FirmeApplication.java
@@ -1,5 +1,7 @@
 package ftn.xmlwebservisi.firme;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,7 +13,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("ftn.xmlwebservisi.firme.repository")
 public class FirmeApplication {
 	
+	private static final Logger logger = LoggerFactory.getLogger(FirmeApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirmeApplication.class, args);
+		logger.info("this is info");
+		logger.debug("this is debug");
 	}
 }
