032a02cb9a3e776405a4f77d24ceafb51213d554
bogdan-todorovic <bogdantodorovic93@gmail.com>
2018-06-27 00:42:22 +0200

    added forgotten dependencies, configured loggers for all packages

--- CertificateGenerator/src/main/java/application/CertificateGeneratorApp.java
+++ CertificateGenerator/src/main/java/application/CertificateGeneratorApp.java
@@ -1,37 +1,20 @@
 package application;
 
-
-
-
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages= {"controller",
-"service", "application"})
-
-
-
+@SpringBootApplication(scanBasePackages= {"controller", "service", "application"})
 public class CertificateGeneratorApp {
 	
-	final static Logger logger = Logger.getLogger(CertificateGeneratorApp.class);
+	final static Logger logger = LoggerFactory.getLogger(CertificateGeneratorApp.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CertificateGeneratorApp.class, args);
 	
-		String parameter = "ASDF";
-		if(logger.isDebugEnabled()){
-			logger.debug("This is debug : " + parameter);
-		}
-		
-		if(logger.isInfoEnabled()){
-			logger.info("This is info : " + parameter);
-		}
-		
-		logger.warn("This is warn : " + parameter);
-		logger.error("This is error : " + parameter);
-		logger.fatal("This is fatal : " + parameter);
-	
+		logger.warn("This is warn");
+		logger.error("This is error");
 	}
 	
 }
